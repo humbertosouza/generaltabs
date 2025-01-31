5f212a2fd1b402e73f6a6a9b588f38884ffbf01b
Humberto Ribeiro <humbertosouza@outlook.com>
2025-01-31 18:03:03 -0500

    Added challenge code

---
+++ challenge1/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from 'next/image';
+
+function Home(){
+	return (
+		<div>
+		<h1>Para Mamae e Julie</h1>
+			<div>Papai fez esse site para treinar e dizer que ama voces!</div>
+			<Image src="/Deia-Beto-Julie-Montreal-2024-12-27.jpg" alt="Deia-Beto-Julie-Montreal-2024-12-27.jpg" width={500} height={600}/>
+		</div>);
+}
+
+export default Home;
+
