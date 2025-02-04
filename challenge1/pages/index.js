975578d97e9438b00728b5c21c4c779e73a8a2cb
Humberto Ribeiro <humbertosouza@outlook.com>
2025-02-04 15:51:01 -0500

    Updated package.json

--- challenge1/pages/index.js
+++ challenge1/pages/index.js
@@ -9,6 +9,7 @@ function Home(){
 		<h1>Para Mamae e Julie</h1>
 			<div>Papai fez esse site para treinar e dizer que ama voces!</div>
 			<div>Mamae, se voce ama o papai, da uma risadinha.</div>
+			<Image src="/Deia-Beto-Julie-Montreal-2024-12-27.jpg" alt="Deia-Beto-Julie-Montreal-2024-12-27.jpg" width={500} height={600}/>
 		</div>);
 }
 
